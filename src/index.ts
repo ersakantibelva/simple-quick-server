9432b27663a82909d6922bf88dc824ef44198428
Ersakanti Belva <ersakantibelva@gmail.com>
2023-06-11 18:04:05 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 import router from './routes/routes'
 import errorHandler from './middlewares/errorHandler'
 
+const allowedOrigins = ['http://localhost:3001'];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
 const app = express()
 
+app.use(cors(options))
 app.use(express.json())
 app.use(router)
 app.use(errorHandler)
